63a3afa504f736abb58246031827fcd22f3d0e00
antonyoni <antony@reduc.tech>
2021-04-30 14:37:51 +0100

    Move interface to separate file

--- ConnectorManager/ConnectorManagerSettings.cs
+++ ConnectorManager/ConnectorManagerSettings.cs
@@ -4,27 +4,6 @@ using System.IO;
 namespace Reductech.EDR.ConnectorManagement
 {
 
-/// <summary>
-/// 
-/// </summary>
-public interface IConnectorManagerSettings
-{
-    /// <summary>
-    /// 
-    /// </summary>
-    string ConnectorPath { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    string ConfigurationPath { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    bool AutoDownload { get; set; }
-}
-
 /// <inheritdoc />
 public class ConnectorManagerSettings : IConnectorManagerSettings
 {
---
+++ ConnectorManager/IConnectorManagerSettings.cs
@@ -0,0 +1,25 @@
+﻿namespace Reductech.EDR.ConnectorManagement
+{
+
+/// <summary>
+/// 
+/// </summary>
+public interface IConnectorManagerSettings
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    string ConnectorPath { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    string ConfigurationPath { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    bool AutoDownload { get; set; }
+}
+
+}
