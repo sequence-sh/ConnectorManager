3639b6c31954d58894c91846f349291cd1606343
antonyoni <antony@reduc.tech>
2021-04-30 14:33:57 +0100

    Update namespace. Remove template tests.

---
+++ ConnectorManager.Tests/ConnectorManagerTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace Reductech.EDR.ConnectorManagement.Tests
+{
+
+public class ConnectorManagerTests
+{
+    [Fact]
+    public void WriteMessage()
+    {
+        Assert.Equal(true, true);
+    }
+}
+
+}
