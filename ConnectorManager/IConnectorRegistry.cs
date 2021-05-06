51cf44d04b3d7b683c35a0d0ac66683fd2c09b98
antonyoni <antony@reduc.tech>
2021-05-06 11:28:07 +0100

    Add documentation

--- ConnectorManager/IConnectorRegistry.cs
+++ ConnectorManager/IConnectorRegistry.cs
@@ -11,11 +11,12 @@ namespace Reductech.EDR.ConnectorManagement
 public interface IConnectorRegistry
 {
     /// <summary>
-    /// 
+    /// Searches connector package IDs for the specified search string.
+    /// Empty string will return all results.
     /// </summary>
-    /// <param name="search"></param>
-    /// <param name="prerelease"></param>
-    /// <param name="ct"></param>
+    /// <param name="search">Search string</param>
+    /// <param name="prerelease">Include prerelease packages when searching</param>
+    /// <param name="ct">The cancellation token. Default is None.</param>
     /// <returns></returns>
     Task<ICollection<ConnectorMetadata>> Find(
         string search,
@@ -23,32 +24,34 @@ public interface IConnectorRegistry
         CancellationToken ct = default);
 
     /// <summary>
-    /// 
+    /// Checks if a connector package with the specified id or id-version exists
+    /// in the connector registry.
     /// </summary>
-    /// <param name="id"></param>
-    /// <param name="version"></param>
-    /// <param name="ct"></param>
+    /// <param name="id">Id (unique name) of the connector package</param>
+    /// <param name="version">Connector version</param>
+    /// <param name="ct">The cancellation token. Default is None.</param>
     /// <returns></returns>
     Task<bool> Exists(string id, string? version = null, CancellationToken ct = default);
 
     /// <summary>
-    /// 
+    /// Returns a list of all versions for the specified connector id.
+    /// The collection will be empty if the id does not exist in the registry.
     /// </summary>
-    /// <param name="id"></param>
-    /// <param name="prerelease"></param>
-    /// <param name="ct"></param>
-    /// <returns></returns>
+    /// <param name="id">Id (unique name) of the connector package</param>
+    /// <param name="prerelease">Include prerelease versions in results</param>
+    /// <param name="ct">The cancellation token. Default is None.</param>
+    /// <returns>A sorted list of versions, with the latest version last.</returns>
     Task<ICollection<string>> GetVersion(
         string id,
         bool prerelease = false,
         CancellationToken ct = default);
 
     /// <summary>
-    /// 
+    /// Retrieves metadata and connector package with the specified id and version.
     /// </summary>
-    /// <param name="id"></param>
-    /// <param name="version"></param>
-    /// <param name="ct"></param>
+    /// <param name="id">Id (unique name) of the connector package</param>
+    /// <param name="version">Connector version</param>
+    /// <param name="ct">The cancellation token. Default is None.</param>
     /// <returns></returns>
     Task<ConnectorPackage> GetConnectorPackage(
         string id,
