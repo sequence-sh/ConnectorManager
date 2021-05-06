c0d58db686399841aca4e87c25bfd1f0a4217701
antonyoni <antony@reduc.tech>
2021-05-06 11:09:14 +0100

    Split ConnectorPackage into two records - package and metadata

--- ConnectorManager/ConnectorPackage.cs
+++ ConnectorManager/ConnectorPackage.cs
@@ -11,31 +11,34 @@ namespace Reductech.EDR.ConnectorManagement
 /// <summary>
 /// 
 /// </summary>
-public record ConnectorPackage(string Id, string Version, MemoryStream? Stream = null)
+public record ConnectorMetadata(string Id, string Version) { }
+
+/// <summary>
+/// 
+/// </summary>
+public sealed record ConnectorPackage
+    (ConnectorMetadata Metadata, PackageArchiveReader Package) : IDisposable
 {
     /// <summary>
     /// 
     /// </summary>
     /// <param name="destination"></param>
     /// <param name="ct"></param>
-    /// <exception cref="ArgumentNullException"></exception>
     public async Task Extract(string destination, CancellationToken ct = default)
     {
-        if (Stream == null)
-            throw new ArgumentNullException(nameof(Stream), "Missing package content");
-
-        using var packageReader = new PackageArchiveReader(Stream);
-
-        var files = await packageReader.GetPackageFilesAsync(PackageSaveMode.Files, ct);
+        var files = await Package.GetPackageFilesAsync(PackageSaveMode.Files, ct);
 
         foreach (var file in files)
         {
             ct.ThrowIfCancellationRequested();
-            var entry       = packageReader.GetEntry(file);
+            var entry       = Package.GetEntry(file);
             var extractPath = Path.Combine(destination, entry.Name);
             entry.ExtractToFile(extractPath);
         }
     }
+
+    /// <inheritdoc />
+    public void Dispose() => Package.Dispose();
 }
 
 }
