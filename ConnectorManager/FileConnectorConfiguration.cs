47a2c4fa7d30d647c40ca84d02f1c3e101305f8d
antonyoni <antony@reduc.tech>
2021-05-06 15:09:54 +0100

    Create base class for ConnectorConfiguration to make testing easier

--- ConnectorManager/FileConnectorConfiguration.cs
+++ ConnectorManager/FileConnectorConfiguration.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Abstractions;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -16,11 +13,69 @@ namespace Reductech.EDR.ConnectorManagement
 /// <summary>
 /// A ConnectorConfiguration that uses a JSON file as the backing store.
 /// </summary>
-public class FileConnectorConfiguration : IConnectorConfiguration
+public class FileConnectorConfiguration : ConnectorConfigurationBase
 {
     private readonly string _path;
-    private readonly Dictionary<string, ConnectorSettings> _connectors;
     private readonly IFileSystem _fileSystem;
+
+    private FileConnectorConfiguration(
+        string path,
+        Dictionary<string, ConnectorSettings> connectors,
+        IFileSystem fileSystem) : base(connectors)
+    {
+        _path       = path;
+        _fileSystem = fileSystem;
+    }
+
+    /// <inheritdoc />
+    public override async Task AddAsync(
+        string name,
+        ConnectorSettings settings,
+        CancellationToken ct = default)
+    {
+        Connectors.Add(name, settings);
+        await SaveSettings(ct);
+    }
+
+    /// <inheritdoc />
+    public override async Task<bool> RemoveAsync(string name, CancellationToken ct = default)
+    {
+        var success = Connectors.Remove(name);
+
+        if (success)
+            await SaveSettings(ct);
+
+        return success;
+    }
+
+    /// <inheritdoc />
+    public override ConnectorSettings this[string name]
+    {
+        get => Connectors[name];
+        set
+        {
+            Connectors[name] = value;
+            SaveSettings(CancellationToken.None).Wait();
+        }
+    }
+
+    private async Task SaveSettings(CancellationToken ct)
+    {
+        var jsonSettings =
+            new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                Converters        = new List<JsonConverter> { EntityJsonConverter.Instance }
+            };
+
+        var output = JsonConvert.SerializeObject(
+            Connectors,
+            Formatting.Indented,
+            jsonSettings
+        );
+
+        await _fileSystem.File.WriteAllTextAsync(_path, output, ct);
+    }
 
     /// <summary>
     /// Create a new ConnectorConfiguration using a JSON file.
@@ -44,106 +99,6 @@ public class FileConnectorConfiguration : IConnectorConfiguration
 
         return new FileConnectorConfiguration(configurationPath, connectors, fileSystem);
     }
-
-    private FileConnectorConfiguration(
-        string path,
-        Dictionary<string, ConnectorSettings> connectors,
-        IFileSystem fileSystem)
-    {
-        _path       = path;
-        _connectors = connectors;
-        _fileSystem = fileSystem;
-    }
-
-    private async Task SaveSettings(CancellationToken ct)
-    {
-        var jsonSettings =
-            new JsonSerializerSettings
-            {
-                NullValueHandling = NullValueHandling.Ignore,
-                Converters        = new List<JsonConverter> { EntityJsonConverter.Instance }
-            };
-
-        var output = JsonConvert.SerializeObject(
-            _connectors,
-            Formatting.Indented,
-            jsonSettings
-        );
-
-        await _fileSystem.File.WriteAllTextAsync(_path, output, ct);
-    }
-
-    /// <inheritdoc />
-    public ICollection<string> Keys => _connectors.Keys;
-
-    /// <inheritdoc />
-    public ICollection<ConnectorSettings> Settings => _connectors.Values;
-
-    /// <inheritdoc />
-    public int Count => _connectors.Count;
-
-    /// <inheritdoc />
-    public async Task AddAsync(
-        string name,
-        ConnectorSettings settings,
-        CancellationToken ct = default)
-    {
-        _connectors.Add(name, settings);
-        await SaveSettings(ct);
-    }
-
-    /// <inheritdoc />
-    public async Task<bool> RemoveAsync(string name, CancellationToken ct = default)
-    {
-        var success = _connectors.Remove(name);
-
-        if (success)
-            await SaveSettings(ct);
-
-        return success;
-    }
-
-    /// <inheritdoc />
-    public bool Contains(string name) => _connectors.ContainsKey(name);
-
-    /// <inheritdoc />
-    public bool ContainsId(string id) =>
-        _connectors.Values.Any(c => c.Id.Equals(id, StringComparison.Ordinal));
-
-    /// <inheritdoc />
-    public bool ContainsVersionString(string id, string version) => _connectors.Values.Any(
-        c => c.VersionString().Equals($"{id} {version}", StringComparison.Ordinal)
-    );
-
-    /// <inheritdoc />
-    public bool TryGetSettings(string name, out ConnectorSettings settings) =>
-        _connectors.TryGetValue(name, out settings!);
-
-    /// <inheritdoc />
-    public bool TryGetSettingsById(string id, out ConnectorSettings[] settings)
-    {
-        settings = _connectors.Values.Where(c => c.Id.Equals(id, StringComparison.Ordinal))
-            .ToArray();
-
-        return settings.Length > 0;
-    }
-
-    /// <inheritdoc />
-    public ConnectorSettings this[string name]
-    {
-        get => _connectors[name];
-        set
-        {
-            _connectors[name] = value;
-            SaveSettings(CancellationToken.None).Wait();
-        }
-    }
-
-    /// <inheritdoc />
-    public IEnumerator<KeyValuePair<string, ConnectorSettings>> GetEnumerator() =>
-        _connectors.GetEnumerator();
-
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
 
 }
