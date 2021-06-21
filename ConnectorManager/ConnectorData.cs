73181f58edf012555707275d333097ba7767c402
antonyoni <antony@reduc.tech>
2021-06-21 16:20:07 +0100

    Add ConnectorData record for storing settings-assembly pairs

---
+++ ConnectorManager/ConnectorData.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Reductech.EDR.ConnectorManagement
+{
+
+/// <summary>
+/// Connector assembly and associated settings.
+/// </summary>
+public record ConnectorData(ConnectorSettings ConnectorSettings, Assembly? Assembly)
+{
+    /// <inheritdoc />
+    public override string ToString() => ConnectorSettings.ToString()!;
+}
+
+}
