7658926689feeb59dc003051c6910d66ea40a582
antonyoni <antony@reduc.tech>
2021-06-21 16:22:06 +0100

    Move ConnectorSettings from Core

---
+++ ConnectorManager/ConnectorSettings.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace Reductech.EDR.ConnectorManagement
+{
+
+/// <summary>
+/// Settings for a connector
+/// </summary>
+[Serializable]
+public class ConnectorSettings
+{
+    /// <summary>
+    /// The id of the connector in the registry
+    /// </summary>
+    [JsonProperty(propertyName: "id")]
+    public string Id { get; set; }
+
+    /// <summary>
+    /// The version of the connector
+    /// </summary>
+    [JsonProperty(propertyName: "version")]
+    public string Version { get; set; }
+
+    /// <summary>
+    /// Whether to enable this connector
+    /// </summary>
+    [JsonProperty(propertyName: "enable")]
+    public bool Enable { get; set; } = true;
+
+    /// <summary>
+    /// Individual settings for the connector
+    /// </summary>
+    [JsonProperty(propertyName: "settings")]
+    public Dictionary<string, object>? Settings { get; set; }
+
+    /// <summary>
+    /// String representation
+    /// </summary>
+    /// <returns></returns>
+    public string VersionString() => $"{Id} {Version}";
+
+    /// <inheritdoc />
+    public override string ToString() => VersionString();
+
+    /// <summary>
+    /// Create default settings for an assembly.
+    /// </summary>
+    public static ConnectorSettings DefaultForAssembly(Assembly assembly) => new()
+    {
+        Id = assembly.GetName().Name ?? "Unknown",
+        Version = FileVersionInfo
+            .GetVersionInfo(assembly.Location)
+            .ProductVersion ?? "Unknown",
+        Enable = true
+    };
+}
+
+}
