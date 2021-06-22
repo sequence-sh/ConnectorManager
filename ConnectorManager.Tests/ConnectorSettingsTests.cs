a28a8ead285a84775671ba9b1591d28a80de03f7
antonyoni <antony@reduc.tech>
2021-06-22 10:41:55 +0100

    Add tests for ConnectorSettings.DefaultForAssembly

---
+++ ConnectorManager.Tests/ConnectorSettingsTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+using Moq;
+using Xunit;
+
+namespace Reductech.EDR.ConnectorManagement.Tests
+{
+
+public class ConnectorSettingsTests
+{
+    [Fact]
+    public void DefaultForAssembly_ByDefault_ReturnsAssemblyInformation()
+    {
+        const string name    = "test";
+        const string version = "0.9.0";
+
+        var assembly = new Mock<Assembly>();
+        assembly.Setup(a => a.GetName()).Returns(() => new AssemblyName(name));
+
+        assembly.Setup(a => a.GetCustomAttributes(It.IsAny<Type>(), true))
+            .Returns(() => new Attribute[] { new AssemblyInformationalVersionAttribute(version) });
+
+        var settings = ConnectorSettings.DefaultForAssembly(assembly.Object);
+
+        Assert.Equal(name,    settings.Id);
+        Assert.Equal(version, settings.Version);
+        Assert.True(settings.Enable);
+    }
+
+    [Fact]
+    public void DefaultForAssembly_WhenNameAndVersionAreNull_ReturnsUnknown()
+    {
+        const string unknown = "Unknown";
+
+        var assembly = new Mock<Assembly>();
+        assembly.Setup(a => a.GetName()).Returns(() => new AssemblyName());
+
+        assembly.Setup(a => a.GetCustomAttributes(It.IsAny<Type>(), true))
+            .Returns(() => new Attribute[] { });
+
+        var settings = ConnectorSettings.DefaultForAssembly(assembly.Object);
+
+        Assert.Equal(unknown, settings.Id);
+        Assert.Equal(unknown, settings.Version);
+    }
+}
+
+}
